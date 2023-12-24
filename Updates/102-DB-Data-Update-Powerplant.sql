4245dd3a1fd6f00b84af5fd8a422123bd5fc021b
Patrick Walters <patrickwalters@hotmail.com>
2023-12-23 23:54:50 -0500

    #2 - Insert Lookup table Powerplant and inserting data

---
+++ Updates/102-DB-Data-Update-Powerplant.sql
@@ -0,0 +1,11 @@
+USE AllThingsAir
+INSERT INTO Powerplant VALUES 
+(101, 'Reciprocating', 'Horizontally-Opposed Engines'),
+(102, 'Reciprocating', 'V-Line Engines'),
+(103, 'Reciprocating', 'In-Line Engines'),
+(104, 'Reciprocating', 'Rotary Engines'),
+(105, 'Reciprocating', 'Radial Engines'),
+(201, 'Turbine', 'Turboprop'),
+(202, 'Turbine', 'Turbofan'),
+(203, 'Turbine', 'Turboshaft'),
+(301, 'Turbine', 'Turbojet')
