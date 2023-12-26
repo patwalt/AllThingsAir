0e0212c37116096048137038e43ac0158542d8a7
Patrick Walters <patrickwalters@hotmail.com>
2023-12-26 00:04:11 -0500

    #4 - Updated the manufacturer to include Lycoming before creating Engines table

--- Updates/102-DB-Data-Update-Powerplants.sql
+++ Updates/102-DB-Data-Update-Powerplants.sql
@@ -1,11 +1,11 @@
 USE AllThingsAir
 INSERT INTO Powerplants VALUES 
-(101, 'Reciprocating', 'Horizontally-Opposed Engines'),
-(102, 'Reciprocating', 'V-Line Engines'),
-(103, 'Reciprocating', 'In-Line Engines'),
-(104, 'Reciprocating', 'Rotary Engines'),
-(105, 'Reciprocating', 'Radial Engines'),
-(201, 'Turbine', 'Turboprop'),
-(202, 'Turbine', 'Turbofan'),
-(203, 'Turbine', 'Turboshaft'),
-(301, 'Turbine', 'Turbojet')
+('Reciprocating', 'Horizontally-Opposed Engines'),
+('Reciprocating', 'V-Line Engines'),
+('Reciprocating', 'In-Line Engines'),
+('Reciprocating', 'Rotary Engines'),
+('Reciprocating', 'Radial Engines'),
+('Turbine', 'Turboprop'),
+('Turbine', 'Turbofan'),
+('Turbine', 'Turboshaft'),
+('Turbine', 'Turbojet')
